e88ea15c52f011f2f44859924cf2afee5befd226
Scarce Koi <111240985+scarcekoi@users.noreply.github.com>
2024-09-28 16:04:28 -0400

    Changed advancement description to "So now you can inhale it." Added two stone cutting recipes for getting powder from the ingots and raw technetium.

--- src/main/java/net/scarcekoi/technetium/datagen/TechnetiumAdvancementProvider.java
+++ src/main/java/net/scarcekoi/technetium/datagen/TechnetiumAdvancementProvider.java
@@ -36,7 +36,7 @@ public class TechnetiumAdvancementProvider extends FabricAdvancementProvider {
                 .display(
                         TechnetiumItems.TECHNETIUM_POWDER,
                         Text.literal("Now It's a Powder"),
-                        Text.literal("And you can inhale it."),
+                        Text.literal("So you can inhale it."),
                         null,
                         AdvancementFrame.TASK,
                         true,
